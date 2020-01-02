88a7bb9efce63dc97f191c64feed1d6e6737ba9a
weverton matias <weverton-matias@hotmail.com>
2020-01-02 12:04:46 -0300

    inserindo a url no ALLOWED_HOSTS

--- econtas/settings.py
+++ econtas/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://e-contas-final.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'e-contas-final.herokuapp.com/']
 
 
 # Application definition
